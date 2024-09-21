dc8c224d6462ae1e5fbe006b0f2ca7d85e3b6a10
Mooling0602 <147964268+Mooling0602@users.noreply.github.com>
2024-09-21 14:14:32 +0800

    Add files via upload

--- sshclient/__init__.py
+++ sshclient/__init__.py
@@ -1,30 +1,37 @@
 from mcdreforged.api.all import *
+from typing import Optional
 import paramiko
+
+ssh_client = None
 
 class SSHClient:
     def __init__(self):
         self.client = None
 
     # 连接到远程服务器
-    def connect(self, hostname, username, password, src: CommandSource):
+    def connect(self, src: CommandSource, hostname, username, password, port):
         if self.client is not None and self.client.get_transport() is not None:
-            src.reply("已经连接到了SSH服务器")
+            src.reply("[SSH]已经连接到了SSH服务器")
         else: 
             try:
-                src.reply(f"正在连接到 {hostname}...")
+                src.reply(f"[SSH]正在连接到 {hostname}...")
                 self.client = paramiko.SSHClient()
                 self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                self.client.connect(hostname, username=username, password=password)
-                src.reply("成功连接到SSH服务器")
+                self.client.connect(hostname, port=port, username=username, password=password)
+                src.reply("[SSH]成功连接到SSH服务器")
+                src.reply("[SSH]SSH会话为全局共享，所有人都可以使用，且同时只能运行一个ssh会话")
+                src.reply("[SSH]若要连接到新的ssh会话，请先断开连接")
             except Exception as e:
-                src.reply(f"连接失败: {e}")
+                src.reply(f"[SSH/错误]连接失败: {e}")
 
     # 执行命令
     def execute(self, command, src: CommandSource):
         if self.client is None or self.client.get_transport() is None:
-            src.reply("请先使用!!ssh connect <地址> <用户名> <密码>进行连接")
+            src.reply("[SSH]请先使用!!ssh connect <地址> <用户名> <密码> <可选：端口>进行连接")
+            src.reply("[SSH]使用!!ssh help查看帮助")
         else:
             try:
+                src.reply("[SSH]正在执行命令（返回结果不带“[SSH]“前缀）")
                 stdin, stdout, stderr = self.client.exec_command(command)
                 output = stdout.read().decode().strip()
                 error = stderr.read().decode().strip()
@@ -33,40 +40,51 @@ class SSHClient:
                 if error:
                     src.reply(error)
             except Exception as e:
-                src.reply(f"执行命令失败: {e}")
-                self.disconnect(src)
+                src.reply(f"[SSH/错误]执行命令失败: {e}")
 
     # 断开连接
-    def disconnect(self, src: CommandSource):
+    def disconnect(self, src: Optional[CommandSource] = None):
         if self.client is None or self.client.get_transport() is None:
-            src.reply("尚未连接到任何SSH服务器")
+            if src is not None:
+                src.reply("[SSH/错误]尚未连接到任何SSH服务器")
 
         else:
             if self.client.get_transport().is_active():
-                src.reply("警告：若有命令正在运行，强制断开会中断它们")
+                if src is not None:
+                    src.reply("[SSH/警告]若有命令正在运行，强制断开会中断它们")
             self.client.close()
             self.client = None
-            src.reply("已断开SSH连接")
+            if src is not None:
+                src.reply("[SSH]已断开SSH连接")
 
 def on_load(server: PluginServerInterface, old_module):
-    ssh_client = SSHClient()
-
+    global ssh_client
+    ssh_client = SSHClient()    
     server.register_command(
         Literal("!!ssh")
-        .runs(lambda src: src.reply("使用!!ssh help查看帮助"))
+        .runs(lambda src: src.reply("[SSH]使用!!ssh help查看帮助"))
         .then(Literal("connect")
               .then(Text("hostname")
                     .then(Text("username")
-                          .then(Text("password")
-                              .runs(lambda src, ctx: ssh_client.connect(ctx["hostname"], ctx["username"], ctx["password"], src))))))
+                        .then(Text("password")
+                            .then(Text("port").suggests(lambda src, ctx: ["22"]))
+                            .runs(lambda src, ctx: ssh_client.connect(src, ctx["hostname"], ctx["username"], ctx["password"], ctx.get("port", 22)))))))
         .then(Literal("disconnect")
               .runs(lambda src: ssh_client.disconnect(src)))
+        .then(Literal("help")
+            .runs(lambda src: help(src)))
         .then(QuotableText("command")
               .runs(lambda src, ctx: ssh_client.execute(ctx["command"], src)))
     )
 
     server.logger.info("SSH客户端插件加载完成")
-    
+
+def help(src: CommandSource):
+    src.reply("[SSH]使用!!ssh connect <地址> <用户名> <密码> <端口>进行连接")
+    src.reply("[SSH]使用!!ssh disconnect断开连接，包括失败的连接")
+    src.reply("[SSH]使用!!ssh \"<命令>\"向远程服务器执行命令，并获取命令执行结果")
+
 def on_unload(server: PluginServerInterface):
-    ssh_client = SSHClient()
-    ssh_client.disconnect(src)
+    global ssh_client
+    ssh_client.disconnect()
+    server.logger.info("[SSH]插件将卸载，现有的ssh会话将被强制断开")
